7cf4e5dd85c85e99e389f368aa788014c9534fb5
fkmstc <fkmstc@gmail.com>
2020-07-06 23:06:53 +0900

    add_10mn

--- hlsjs/demo/js/editHLSjs.js
+++ hlsjs/demo/js/editHLSjs.js
@@ -9,6 +9,7 @@ function newHls() {
 
 					//tsファイルを探しだす
 					var tsfilename = context.frag.relurl;
+					//console.log(context);
 					for (i = 0; i < USERJSON.length; i++) {
 						if (tsfilename == USERJSON[i].tsFileName) {
 							count = i;
@@ -20,19 +21,24 @@ function newHls() {
 					if (USERJSON[count].URL == "Server") {
 						console.log("サーバからデータを取得")
 						//console.log(context)
-						
+
 						//アクセス先のURLを動的変更
 						context.url = `./hls/${USERJSON[count].dirName}/${USERJSON[count].tsFileName}`;
+						// ./hls/  5min_PlayList1          / 5min_Ts010.ts
 						//console.log(context.url)
 
 						if (true) {
 							var onSuccess = callbacks.onSuccess;
 							callbacks.onSuccess = function (response, stats, context) {
 								//DOM表示
-								$("#info").html( $("#info").html() + `Server:${USERJSON[count].dirName[13]}  ${USERJSON[count].tsFileName.substr( 7, 12 )}` + "<br>")
-								//受け取ったデータ(response.data)に対しURLを発行 sessionストレージに保存
-								var blob = new Blob([response.data],{type:"video/mp2t"})
-								sessionStorage.setItem(USERJSON[count].tsFileName,URL.createObjectURL(blob));
+								$("#info").html($("#info").html() + `Server:${USERJSON[count].dirName[13]}  ${USERJSON[count].tsFileName.substr(7, 12)}` + "<br>")
+								//Server:1            ts_00x.ts
+
+								//サーバから受け取った動画データ(response.data)に対しURLを発行 sessionストレージに保存
+								//バイナリデータに変換
+								var blob = new Blob([response.data], { type: "video/mp2t" })
+								//セッションストレージに保存
+								sessionStorage.setItem(USERJSON[count].tsFileName, URL.createObjectURL(blob));
 								onSuccess(response, stats, context);
 							}
 						}
@@ -40,12 +46,13 @@ function newHls() {
 					};
 
 					//peerなら
-					if(USERJSON[count].URL != "Server"){
+					if (USERJSON[count].URL != "Server") {
 						//data[0:相手のpeerid][1:tsファイル名][2:自分のpeerid]
-						PeerDataSendFunction([USERJSON[count].URL,USERJSON[count].tsFileName,id]).then(function(){
-							context.url = sessionStorage.getItem(USERJSON[count].tsFileName)
-							load(context, config, callbacks);
-						})
+						PeerDataSendFunction([USERJSON[count].URL, USERJSON[count].tsFileName, id])
+							.then(function () {
+								context.url = sessionStorage.getItem(USERJSON[count].tsFileName)
+								load(context, config, callbacks);
+							})
 					}
 
 					this.abort = function () {
@@ -55,12 +62,24 @@ function newHls() {
 			}
 		}
 
+
 		var hls = new Hls({
 			"debug": true,
+			//"debug": false,
 			fLoader: fLoader
 		});
 		var video = document.getElementById('video');
-		hls.loadSource("./hls/BasePlayList.m3u8");
+		hls.loadSource("./hls/BasePlayList2.m3u8");
+		//hls.loadSource("./hls/BasePlayList.m3u8");
 		hls.attachMedia(video);
+
+		function onLevelLoaded(event, data) {
+			var level_duration = data.details;
+			console.log(level_duration)
+		}
+		// subscribe event
+		hls.on(Hls.Events.LEVEL_LOADED, onLevelLoaded);
+		hls.on(Hls.Events.LEVEL_PTS_UPDATED, onLevelLoaded);
+
 	}
 };
--- hlsjs/demo/js/skywayEdit.js
+++ hlsjs/demo/js/skywayEdit.js
@@ -2,42 +2,68 @@
 //相手から接続された
 function  PeerDataReceiveFunction(){
 	peer.on('connection', dataConnection => {
+
+
 		dataConnection.once('open', () => {
 			console.log("相手から接続されました");
 		})
+
+
+		//[0:tsファイル名,1:データが欲しいと言ったpeerID]
 		dataConnection.on('data', data => {
 			//DOM表示
 			$("#info").html($("#info").html() + data[1] + "へ" + data[0].substr( 7, 12 ) + "を送信<br>");
+
 			var xhr = new XMLHttpRequest();
 			xhr.open("GET", sessionStorage.getItem(data[0]));
 			xhr.send(null);
 			xhr.responseType = "blob";
+
 			xhr.onload = function () {
-				
+				//ここで相手にデータを送信
+				//[0:tsファイル名,1:バイナリデータ,2:自分のID]
 				dataConnection.send([data[0], xhr.response, id]);
 			}
 		})
+
+
 		dataConnection.once('close', () => {
 			console.log("相手が接続を切りました");
 		})
+
+
+
+
 	})
 }
 
 //data[0:相手のpeerid][1:tsファイル名][2:自分のpeerid]
 function PeerDataSendFunction(data) {
 	return new Promise(function (resolve) {
+
 		console.log(`${data[0]}へ接続`)
+
+		//相手のピアに接続した時の情報
 		var dataConnection = peer.connect(data[0]);
+
 		dataConnection.once('open', () => {
 			console.log("自分がpeerに接続しました");
+			//相手のピアに欲しいtsファイル名と自分のpeerIDを送信
+			//[0:tsファイル名,1:データが欲しいと言ったpeerID]
 			dataConnection.send([data[1], data[2]]);
 		})
+
+		//データの受け取り
+		//[0:tsファイル名,1:バイナリデータ,2:送信した相手のID]
 		dataConnection.on('data', data => {
-			console.log(data[1]);
+			//console.log(data[0]);
 			var blob = new Blob([data[1]], { type: "video/mp2t" });
 			var url = URL.createObjectURL(blob);
 			sessionStorage.setItem(data[0], url);
+			//DOM操作
 			$("#info").html($("#info").html() + data[2] + ":" + data[0].substr( 7, 12 ) + "<br>");
+			
+			//終わったよっていう信号
 			resolve();
 		})
 	})
